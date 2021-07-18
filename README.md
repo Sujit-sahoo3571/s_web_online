d5db8d8925c44cd12ff01a379e149fc58cdb9384
Sujit-sahoo3571 <55569677+Sujit-sahoo3571@users.noreply.github.com>
2021-07-18 15:22:45 +0530

    Revert "added burger menu"

--- README.md
+++
@@ -1,4 +0,0 @@
-# s_web_online
-for publication websites 
-All Websites &copy; sujit
-i'm learning and this is just the beginning :)
--- app.js
+++
@@ -1,4 +0,0 @@
-function displayBurgerMenu(){  
-    let burgerMenu=document.querySelector(".burgerMenu")    
-    burgerMenu.classList.toggle("activeBurgMenu")                
-}
